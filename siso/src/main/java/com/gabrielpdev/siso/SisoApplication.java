b8796253a3caa27ad16fdc1f62531b1e53eed9aa
gabrielpandrade <pintoandradegabriel@gmail.com>
2024-08-07 00:13:25 -0300

    add: Adicionando backend ao projeto

---
+++ siso/src/main/java/com/gabrielpdev/siso/SisoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielpdev.siso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisoApplication.class, args);
+	}
+
+}
---
+++ siso/src/test/java/com/gabrielpdev/siso/SisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpdev.siso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
