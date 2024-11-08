3cd6c58edfaee9c330826ea7c18aaa1a6a2cc9cb
gabrielpandrade <pintoandradegabriel@gmail.com>
2024-11-07 23:08:14 -0300

    refactor: receita e despesa

--- siso/src/main/java/com/gabrielpdev/siso/dtos/TipoDespesaDTO.java
+++ siso/src/main/java/com/gabrielpdev/siso/dtos/TipoDespesaDTO.java
@@ -1,6 +1,7 @@
 package com.gabrielpdev.siso.dtos;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import lombok.Value;
 
 import java.io.Serializable;
@@ -8,7 +9,7 @@ import java.io.Serializable;
 /**
  * DTO for {@link com.gabrielpdev.siso.models.TipoDespesa}
  */
-@Value
+@Data
 public class TipoDespesaDTO implements Serializable {
     @NotNull
     String descricao;
